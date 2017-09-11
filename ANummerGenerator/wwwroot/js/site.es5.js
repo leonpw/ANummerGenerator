99cc5279f1a1f8d4480e5bb7bf937f574a539330
Leon de Pruyssenaere de la Woestijne <leonpw@outlook.com>
2017-09-11 14:46:27 +0200

    minified es5 files

--- ANummerGenerator/wwwroot/js/site.es5.js
+++ ANummerGenerator/wwwroot/js/site.es5.js
@@ -1,23 +1,8 @@
-﻿// Write your Javascript code.
+﻿'use strict';
 
-'use strict';
-
-$(function () {
-
-    $('#Generate').on('click', function () {
-        var amount = $('#amount').val();
-        $.ajax({
-            url: "/api/Anummer/" + amount,
-            success: function success(result) {
-                result.forEach(function (item) {
-                    $('#results ol').append('<li>' + item + '</li>');
-                });
-            },
-            error: function error(_error) {
-                alert(_error.text);
-            }
-
-        });
+function CompleteSendFunctionName(jqXHR, textStatus) {
+    jqXHR.responseJSON.forEach(function (item) {
+        $('#results ol').append('<li>' + item + '</li>');
     });
-});
+}
 
